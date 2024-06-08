5b3f9bd67b588d836ca13786d3eba04af310da4c
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-06-08 09:07:47 +0300

    init the project

---
+++ MessengerSwiftUI/MessengerSwiftUI/MessengerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerSwiftUIApp.swift
+//  MessengerSwiftUI
+//
+//  Created by Oleksandr Isaiev on 08.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct MessengerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
