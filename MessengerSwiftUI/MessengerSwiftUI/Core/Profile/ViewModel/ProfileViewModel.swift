1c460338db9bb96a1cd3c131fb35680badae7f65
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-06-10 08:29:31 +0300

    add profile viewModel

---
+++ MessengerSwiftUI/MessengerSwiftUI/Core/Profile/ViewModel/ProfileViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  ProfileViewModel.swift
+//  MessengerSwiftUI
+//
+//  Created by Oleksandr Isaiev on 09.06.2024.
+//
+
+import SwiftUI
+import PhotosUI
+
+final class ProfileViewModel: ObservableObject {
+    @Published var selectedItem: PhotosPickerItem? {
+        didSet {
+            Task { try await loadImage() }
+        }
+    }
+
+    @Published var profileImage: Image?
+
+    func loadImage() async throws {
+        guard let item = selectedItem else { return }
+        guard let imageData = try await item.loadTransferable(type: Data.self) else { return }
+        guard let uiImage = UIImage(data: imageData) else { return }
+        self.profileImage = Image(uiImage: uiImage)
+    }
+}
